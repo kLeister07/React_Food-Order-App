90749b03645d89c4ac84fd7195558f7aedbb99c0
Kevin Leister <kevinleister07@gmail.com>
2022-10-04 13:59:04 -0500

    add functions for cart, add remove items and total

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import classes from './headercartbutton.module.css'
 import CartIcon from '../Cart/CartIcon';
+import CartContext from '../../store/cart-context';
 
 const HeaderCartButton = (props) => {
+    const cartCtx = useContext(CartContext);
+    
+    const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+        return curNumber + item.amount;
+    }, 0);
+
   return (
-      <button className={classes.button} >
+      <button className={classes.button} onClick={props.onClick}>
           <span className={classes.icon} >
               <CartIcon />
           </span>
@@ -12,7 +19,7 @@ const HeaderCartButton = (props) => {
               Your Cart
           </span>
           <span className={classes.badge}>
-              3
+              {numberOfCartItems}
           </span>
       </button>
   );
